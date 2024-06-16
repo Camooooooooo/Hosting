4a7e3e57edef5ec80415f573e328e8dc3b0ea860
Camooooooooo <pixelinteractive2022@gmail.com>
2024-06-16 12:01:01 -0500

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["Watching Avalon Interactive","Managing Avalon Interactive"];
 
 
 let currentIndex = 0;
